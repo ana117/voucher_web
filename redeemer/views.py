600d91d9ae3ec23c271e361fe5cfb2aef1128265
ana117 <63540951+ana117@users.noreply.github.com>
2022-06-23 13:19:39 +0700

    Extract redeem voucher into its own function

--- redeemer/views.py
+++ redeemer/views.py
@@ -1,20 +1,33 @@
+from datetime import datetime
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from models.models import Voucher
 
 REDEEMER_PAGE = "redeemer-page.html"
 
 
+def redeem(voucher_code):
+    voucher = Voucher.objects.get(code=voucher_code)
+    if voucher.is_used:
+        return False
+
+    voucher.is_used = True
+    voucher.date_used = datetime.now()
+    voucher.save()
+    return True
+
+
 def redeem_page(request):
     if request.method == "POST":
         voucher_code = request.POST.get("voucher_code")
         try:
-            voucher = Voucher.objects.get(code=voucher_code)
-            if voucher.is_used:
+            successfully_redeemed = redeem(voucher_code)
+            if successfully_redeemed:
+                return render(request, REDEEMER_PAGE, {"success": f"Code {voucher_code} has been redeemed!"})
+            else:
                 return render(request, REDEEMER_PAGE, {"error": "Voucher has been used"})
 
-            voucher.is_used = True
-            voucher.save()
-            return render(request, REDEEMER_PAGE, {"success": f"Code {voucher_code} has been redeemed!"})
         except Voucher.DoesNotExist:
             return render(request, REDEEMER_PAGE, {"error": "Code not found"})
 
