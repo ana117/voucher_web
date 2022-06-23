c8045b2ac74e5e4063a5558a296bc5f71bc5867b
ana117 <63540951+ana117@users.noreply.github.com>
2022-06-23 13:19:08 +0700

    Add date_used on Voucher model

--- models/models.py
+++ models/models.py
@@ -5,3 +5,4 @@ class Voucher(models.Model):
     code = models.CharField(max_length=12, primary_key=True)
     description = models.TextField()
     is_used = models.BooleanField()
+    date_used = models.DateTimeField(null=True)
